2425b14a47e10a76ef7022b24ebd0c2525d3f170
VictorLima <victoraugustolima1@gmail.com>
2023-02-15 19:03:36 -0300

    Projeto Iniciado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sorveteriaMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ context/AppDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace sorveteriaMVC.context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        
+    }
+    
+}
